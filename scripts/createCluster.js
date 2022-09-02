dcec988e199dd384d6b694c2a2d6b811e85e7cdf
SiryjVyiko <dz@hivext.net>
2022-09-02 11:51:49 +0300

    Create createCluster.js

---
+++ scripts/createCluster.js
@@ -0,0 +1,21 @@
+var regions = '${settings.regions}'.split(','), masterNodesString = "", createClusterCommand = "", resp,
+    getAnnounceIpCommand = "cat /etc/redis.conf|grep ^cluster-announce-ip|tail -n 1|awk '{print $2}'",
+    targetMasterIdCommand = "export REDISCLI_AUTH=$(cat /etc/redis.conf |grep '^requirepass'|awk '{print $2}'); redis-cli cluster nodes|grep myself|awk '{print $1}'";    
+for (var cluster = 1, n = regions.length + 1; cluster < n; cluster++) {
+    var envInfo = jelastic.env.control.GetEnvInfo('${settings.mainEnvName}-' + cluster, session);
+    if (envInfo.result != 0) {
+        return envInfo;
+    }
+    var resp = jelastic.env.control.ExecCmdById('${settings.mainEnvName}-' + cluster, session, envInfo.nodes[0].id, toJSON([{"command": getAnnounceIpCommand, "params": ""}]), false, "root");
+    if (resp.result != 0) { return resp; }
+    announceIp = resp.responses[0].out
+    masterNodesString = masterNodesString + announceIp + ":6379 "
+}
+
+createClusterCommand = "export REDISCLI_AUTH=$(cat /etc/redis.conf |grep '^requirepass'|awk '{print $2}'); redis-cli --cluster create  " + masterNodesString + " --cluster-replicas 1";
+resp = jelastic.env.control.ExecCmdById('${settings.mainEnvName}-' + cluster, session, envInfo.nodes[0].id, toJSON([{"command": createClusterCommand, "params": ""}]), false, "root");
+if (resp.result != 0) { return resp; }    
+
+return {
+    result: 0
+};
