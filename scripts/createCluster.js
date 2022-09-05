66016599c0f31060e800e80510535c1524f7f80a
SiryjVyiko <dz@hivext.net>
2022-09-05 11:29:24 +0300

    Update createCluster.js

--- scripts/createCluster.js
+++ scripts/createCluster.js
@@ -16,4 +16,30 @@ envInfo = jelastic.env.control.GetEnvInfo('${settings.mainEnvName}-1', session);
 if (envInfo.result != 0) { return envInfo; }
 createClusterCommand = "export REDISCLI_AUTH=$(cat /etc/redis.conf |grep '^requirepass'|awk '{print $2}'); yes yes | redis-cli --cluster create  " + masterNodesString + " --cluster-replicas 0";
 resp = jelastic.env.control.ExecCmdById('${settings.mainEnvName}-1', session, envInfo.nodes[0].id, toJSON([{"command": createClusterCommand, "params": ""}]), false, "root");
-return resp; 
+if (resp.result != 0) { return resp; }
+
+
+
+for (var cluster = 1, n = regions.length + 1; cluster < n; cluster++) {
+    var resp = jelastic.env.control.GetEnvInfo('${settings.mainEnvName}-' + cluster, session);
+    if (resp.result != 0) {
+        return resp;
+    }
+    for (var i = 1, k = resp.nodes; i < k.length; i++) {
+        if (k[i].nodeGroup == 'nosqldb') {
+            var resp = jelastic.env.control.ExecCmdById('${settings.mainEnvName}-' + cluster, session, k[i].id, toJSON([{"command": getAnnounceIpCommand, "params": ""}]), false, "root");
+            if (resp.result != 0) { return resp; }
+            announceIp = resp.responses[0].out
+            var meetCommand = "export REDISCLI_AUTH=$(cat /etc/redis.conf |grep '^requirepass'|awk '{print $2}'); redis-cli -h 127.0.0.1 cluster meet " + announceIp + " 6379; sleep 15";
+            resp = jelastic.env.control.ExecCmdById('${settings.mainEnvName}-' + cluster, session, k[0].id, toJSON([{"command": meetCommand, "params": ""}]), false, "root");
+            if (resp.result != 0) { return resp; }
+        }
+    }
+}
+
+
+
+
+return {
+    result: 0
+};
